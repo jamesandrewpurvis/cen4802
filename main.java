e04a446d7cb866890abd33decaf9c3c183ed3569
jamesandrewpurvis <84031772+jamesandrewpurvis@users.noreply.github.com>
2021-08-30 14:09:17 -0600

    Added javadoc comments to file

--- main.java
+++ main.java
@@ -7,6 +7,13 @@ public class main {
 
 	}
 	
+	/**
+	 * A method that runs recursively and solves the fibonacciSequence for the nthTerm it adds n - 1 + n - 2 until the nthTerm is found.
+	 * @param n (The number we will use to find the nthTerm)
+	 * @author James Purvis
+	 * @return nthTerm  (The sum of the digits used to make n)
+	 */
+	
 	public static int fibonacciSequence(int n)
 	{
 		if (n <= 1)
